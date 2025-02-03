baf55a760fa1f8f9024423c248287d99184c28bc
ashdev-7 <yash23tyagi@gmail.com>
2025-02-03 18:01:06 +0530

    Minor changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: "export",
+    images: {
+        unoptimized: true,
+    },
+    basePath: "/portfolio",
+    assetPrefix: "/portfolio/"
+};
+
+module.exports = nextConfig;
