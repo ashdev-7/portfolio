1c6f1782a5938734a1d2acf2577736dfc8302405
ashdev-7 <yash23tyagi@gmail.com>
2025-02-03 18:58:28 +0530

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,17 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
-
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: "export",
-    images: {
-        unoptimized: true,
-    },
-    basePath: "/portfolio",
-    assetPrefix: "/portfolio/"
-};
+const nextConfig = {};
 
 export default nextConfig;
+
+// // NOT Working on local machine
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     output: "export",
+//     images: {
+//         unoptimized: true,
+//     },
+//     basePath: "/portfolio",
+//     assetPrefix: "/portfolio/"
+// };
+
+// export default nextConfig;
